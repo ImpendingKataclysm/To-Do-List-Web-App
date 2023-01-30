f7f68d98c01d326daf8732cf407f922220b1c310
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-01-30 12:33:18 -0800

    Reverted some changes to address errors

--- web.py
+++ web.py
@@ -1,36 +1,26 @@
 import streamlit as st
 import functions as f
 
-tasks = f.get_list_from_file(f.TODO_FILEPATH)
+tasks = f.get_list_from_file()
 tasks = [t.title() for t in tasks]
-
-shopping = f.get_list_from_file(f.SHOPPING_FILEPATH)
-shopping = [s.title() for s in shopping]
 
 
 def add_task():
     todo = st.session_state["new_task"].title() + '\n'
     tasks.append(todo)
-    f.write_list_to_file(tasks, f.TODO_FILEPATH)
+    f.write_list_to_file(tasks)
 
 
-def add_shopping_item():
-    shopping_item = st.session_state["new_shopping_item"].title() + '\n'
-    shopping.append(shopping_item)
-    f.write_list_to_file(shopping, f.SHOPPING_FILEPATH)
+st.title("My To-Do List App")
+st.subheader("Increase your productivity")
 
+for index, task in enumerate(tasks):
+    checkbox = st.checkbox(task, key=task)
+    if checkbox:
+        tasks.pop(index)
+        f.write_list_to_file(tasks)
+        del st.session_state[task]
+        st.experimental_rerun()
 
-st.title("Personal Organizer")
-
-with st.expander("To-Do List"):
-    for index, task in enumerate(tasks):
-        checkbox = st.checkbox(task, key=task)
-        if checkbox:
-            tasks.pop(index)
-            f.write_list_to_file(tasks, f.TODO_FILEPATH)
-            del st.session_state[task]
-            st.experimental_rerun()
-
-    st.text_input(label="Enter a new task: ",
-                  on_change=add_task, key="new_task")
-
+st.text_input(label="Enter a new task: ",
+              on_change=add_task, key="new_task")
